4acff655dbeb780293fd0c4bcd86ed207ead3a43
Helena Chen <96384004+ChenHelena@users.noreply.github.com>
2025-01-04 10:40:35 +0800

    Update index.js

--- week1/index.js
+++ week1/index.js
@@ -8,6 +8,9 @@
 
 // 1.1 範例
 const alexAge = 25;
+const alexMembershipID = "GYM2024-12345";
+const isRunningOnTreadmill = true;
+console.log(alexAge, alexMembershipID, isRunningOnTreadmill) // 25, "GYM2024-12345", true
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,28 +19,44 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaGroupClass = 300;
+const retrainingGroupClass = 500;
+const retrainingOneOnOne = 1500;
+const sportsBudget = 3000;
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
-// 條件一：一定至少要買 1 堂重訓團課和瑜伽團課
-// 條件二：瑜伽團課只能一次買 3 堂
-// 條件三：一定要花到 2400 以上，並購買 5 堂課程
+let AlexBudget;
 
-console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
+// 條件一：一定至少要買 1 堂重訓團課和瑜伽團課
+AlexBudget = sportsBudget - retrainingGroupClass - yogaGroupClass;
+
+// 條件二：瑜伽團課只能一次買 3 堂
+AlexBudget -= yogaGroupClass*2;
+
+// 條件三：一定要花到 2400 以上，並購買 5 堂課程
+AlexBudget -= retrainingOneOnOne;
+
+console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`); // 剩下100
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
+let classPackage = 2520;
+let courseTime = 50;
+let oneLesson = 180;
 
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
+const redLight = 'red';
+const greenLight = 'green';
+let waitingTime = 28;
 // 4-2. 目前一起等待的機車有 8 台
+let waitingScooter = 8;
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+let cloud = 5;
+const sun = 1;
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,8 +69,11 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+myWater -= 800; // 中午喝了 800cc
+myWater += 1000; // 裝了 1000cc
+myWater -= 700; // 喝了 700cc
 
-console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
+console.log(`Alex 的水壺還有 ${myWater}cc 的水`); // 1000cc
 
 // ### 題目七：情境題：變數計算
 // 情境：Anna 每週都會到單次計費型的健身房運動，週日運動結束後，想知道自己本週的消費金額，但結帳系統出了點問題，Anna 決定自己用 JS 來計算。
@@ -62,40 +84,45 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
-
+const groupLesson = 150;
+totalBill = machineUsePrice*3 + groupLesson*2
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
-);
+);// 150元, 300元, 450元
 
 // ### 題目八：變數重新賦予值
 // 情境：請依照以下程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 let a = 8; // 範例：宣告了一個 a 的變數，並賦予了一個 8 的數字型別
-let b = 0;
-a = 13;
-a = b + 4;
-a - b;
-b += 1;
+let b = 0; // 宣告了一個 b 的變數，並賦予了一個 0 的數字型別
+a = 13; // a 變數重新賦予 13 的數字型別
+a = b + 4; // a 變數重新賦予 4 的數字型別
+a - b; // 運算 4-0 不改變 a,b的值
+b += 1; // b 變數重新賦予 1 的數字型別
 
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
-let c = 'world';
-let d = 456;
-let e = c + d;
-let f = false;
-let g = d + d;
-let h = f + g;
+let c = 'world'; // 字串
+let d = 456; // 數字
+let e = c + d; // 字串
+let f = false; // boolean
+let g = d + d; // 數字
+let h = f + g; // 數字
 
 // 請從以下新增註解，告知上面每行各別是哪些型別
-// a 是 string
-// b 是 ???
+// c 是 string
+// d 是 number
+// e 是 string
+// f 是 boolean
+// g 是 number
+// h 是 數字
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 
-let numberArr1 = [5, 10, 15];
-let numberArr2 = numberArr1;
-numberArr2.push(20);
-numberArr2 = [25, 30, 35];
-console.log(numberArr1, numberArr2);
+let numberArr1 = [5, 10, 15]; // 宣告 numberArr1 並賦予 [5, 10, 15] 陣列
+let numberArr2 = numberArr1; // 宣告 numberArr2 指向與 numberArr1 同個記憶體位置
+numberArr2.push(20); // 在 numberArr2 陣列插入 20
+numberArr2 = [25, 30, 35]; // numberArr2 指向一個新的陣列 [25, 30, 35] 不再指向 numberArr1 的記憶體位置
+console.log(numberArr1, numberArr2); // [5, 10, 15, 20], [25, 30, 35]
